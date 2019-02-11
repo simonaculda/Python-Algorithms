63d0a050b894e37df9cd39ace37c613a114ef1aa
simonaculda <simonaculda95@gmail.com>
2019-02-11 13:22:48 +0000

    Python algorithms

--- findPivotIndex.py
+++ findPivotIndex.py
@@ -4,6 +4,7 @@ We define the pivot index as the index where the sum of the numbers to the left 
 to the sum of the numbers to the right of the index.
 If no such index exists, we should return -1. If there are multiple pivot indexes,
 you should return the left-most pivot index.
+
 https://leetcode.com/problems/find-pivot-index/
 """
 
